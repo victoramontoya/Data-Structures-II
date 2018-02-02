a0171d1ee37dbe39e2fafb64c22b05a556930490
Victor Montoya <victoramontoya@gmail.com>
2018-02-01 20:27:16 -0800

    Victor Montoya -Finished

--- src/graph.js
+++ src/graph.js
@@ -40,20 +40,40 @@ class Graph {
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
-    this._value = value;
-  //  if (this.vertices <= 2)
+    const newNode = new GraphNode({ value, edges });
+    edges.forEach((edge) => {
+      this.addEdge(newNode, edge);
+    });
+    if (this.vertices.length === 1) {
+      this.addEdge(this.vertices[0], newNode);
+    }
+    this.vertices.push(newNode);
+    return newNode;
   }
   // Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-    this._value = value;
-    return this.value[value] ? true : false;
+    for (let i = 0; i < this.vertices.length; i++) {
+      if (this.vertices[i].value === value) {
+        return true;
+      }
+    }
+    return false;
   }
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
-    this._value = value;
+    this.vertices = this.vertices.reduce((acc, vertex) => {
+      if (vertex.value === value) {
+        vertex.edges.forEach((edge) => {
+          this.removeEdge(vertex, edge);
+        });
+        return acc;
+      }
+      acc.push(vertex);
+      return acc;
+    });
   }
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
@@ -61,27 +81,47 @@ class Graph {
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
-    this._value = fromVertex;
-    if (this._value[this._value] && this._value[toVertex]) {
-      return true;
-    }
+        // short hand from above
+    const test = this.value;
+    return (toVertex.edges.includes(fromVertex) && fromVertex.edges.includes(toVertex));
   }
+    // long hand
+    // const fromEdges = fromVertex.edges;
+    // const toEdges = toVertex.edges;
+    // const fromContains = false;
+    // const toContains = false;
+    // for (let i = 0; i < fromEdges.length; i++) {
+    //   if (fromEdges[i].value === toVertex.value) {
+    //     fromContains = true;
+    //   }
+    // }
+    // for (let i = 0; i < ftoEdges.length; i++) {
+    //   if (toEdges[i].value === fromVertex.value) {
+    //     toContains = true;
+    //   }
+    // }
+    // return fromContains && toContains;
+  
   // Adds an edge between the two given vertices if no edge already exists between them
   // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
-    this._value = fromVertex;
-    // this._edges[this._value][toVertex] = true;
-    // this._edges[toVertex][this._value] = true;
-    // this.edges[this._value] = [this._value, toVertex];
-    this._value.edges = [this._value];
-    this._value.pushToEdges(toVertex);
+    if (!this.checkIfEdgeExists(fromVertex, toVertex)) { 
+      fromVertex.pushToEdges(toVertex);
+      toVertex.pushToEdges(fromVertex);
+    }
   }
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
   // If a vertex would be left without any edges as a result of calling this function, those
   // vertices should be removed as well
   removeEdge(fromVertex, toVertex) {
-    this._value = fromVertex;
+    fromVertex.edges = fromVertex.edges.filter((edge) => { return edge.value !== toVertex.value; });
+    toVertex.edges = toVertex.edges.filter((edge) => { return edge.value !== fromVertex.value; });
+
+    this.vertices = this.vertices.reduce((acc, vertex) => {
+      if (vertex.edges.length !== 0) acc.push(vertex);
+      return acc;
+    }, []);
   }
 }
 
