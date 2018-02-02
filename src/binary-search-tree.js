56b1caf8408ee5689515bbce4a3f34ed8e6ea7a6
Victor Montoya <victoramontoya@gmail.com>
2018-02-01 17:55:27 -0800

    Victor Montoya - All But Graphs

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,6 +2,8 @@
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+const Queue = require('./queue-helper');
+
 class BinarySearchTree {
   constructor(value) {
     this.value = value;
@@ -12,18 +14,34 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if (value <= this.value) {
+      if (!this.left) this.left = new BinarySearchTree(value);
+      else this.left.insert(value);
+    } else if (value > this.value) {
+      if (!this.right) this.right = new BinarySearchTree(value);
+      else this.right.insert(value);
+    }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+    if (this.value === target) return true;
+    if (target <= this.value) {
+      if (!this.left) return false;
+      return this.left.contains(target);
+    } else if (target > this.value) {
+      if (!this.right) return false;
+      return this.right.contains(target);
+    }
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
-
+    this.value = cb(this.value);
+    if (this.left) this.left.depthFirstForEach(cb);
+    if (this.right) this.right.depthFirstForEach(cb);
   }
+  
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
@@ -31,7 +49,22 @@ class BinarySearchTree {
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
   breadthFirstForEach(cb) {
-
+    const newQueue = new Queue();
+    newQueue.enqueue(this);
+    while (!newQueue.isEmpty()) {
+      const node = newQueue.dequeue();
+      if (node.left) newQueue.enqueue(node.left);
+      if (node.right) newQueue.enqueue(node.right);
+      cb(node.value);
+    }
+    // const newQueue = new Queue();
+    // newQueue.enqueue(cb(this));
+    // while (newQueue.length > 0) {
+    //   const node = newQueue.dequeue();
+    //   for (let i = 0, c = node.this.length; i < c; i++) {
+    //     return newQueue.enqueue(node.this[i]);
+    //   }
+    // }
   }
 }
 
--- src/graph.js
+++ src/graph.js
@@ -40,18 +40,20 @@ class Graph {
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
-
+    this._value = value;
+  //  if (this.vertices <= 2)
   }
   // Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-
+    this._value = value;
+    return this.value[value] ? true : false;
   }
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
-
+    this._value = value;
   }
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
@@ -59,19 +61,27 @@ class Graph {
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
-
+    this._value = fromVertex;
+    if (this._value[this._value] && this._value[toVertex]) {
+      return true;
+    }
   }
   // Adds an edge between the two given vertices if no edge already exists between them
   // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
-
+    this._value = fromVertex;
+    // this._edges[this._value][toVertex] = true;
+    // this._edges[toVertex][this._value] = true;
+    // this.edges[this._value] = [this._value, toVertex];
+    this._value.edges = [this._value];
+    this._value.pushToEdges(toVertex);
   }
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
   // If a vertex would be left without any edges as a result of calling this function, those
   // vertices should be removed as well
   removeEdge(fromVertex, toVertex) {
-
+    this._value = fromVertex;
   }
 }
 
